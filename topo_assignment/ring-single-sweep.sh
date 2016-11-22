bb1acde6eff342de48614a02f0cd857c4936f09d
yangqiyu <patrick.yangqiyu@gmail.com>
2016-11-22 20:48:13 +0800

    Topo assignment

---
+++ topo_assignment/ring-single-sweep.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Function: run the ring topology for various
+# value of N
+
+# Exit on any failure
+set -e
+
+# Check for uninitialized variables
+set -o nounset
+
+ctrlc() {
+	killall -9 python
+	mn -c
+	exit
+}
+
+trap ctrlc SIGINT
+
+start=`date`
+exptid=`date +%b%d-%H:%M`
+
+bw=10
+delay='1ms'
+loss=0
+rootdir=Ring-$exptid-Single-Delay$delay-Loss$loss
+# Note: you need to make sure you report the results
+# for the correct port!
+# In this example, we are assuming that each
+# client is connected to port 2 on its switch.
+
+for n in  4 5 6; do
+    dir=$rootdir/n$n
+    python ringsingle.py --bw $bw \
+        --dir $dir \
+        --delay $delay\
+        --loss $loss\
+        -t 30 \
+        -n $n
+    python util/plot_rate.py --rx \
+        --maxy $bw \
+        --xlabel 'Time (s)' \
+        --ylabel 'Rate (Mbps)' \
+        -i 's.*-eth1' \
+        -f $dir/bwm.txt \
+        -o $dir/rate.png
+    python util/plot_tcpprobe.py \
+        -f $dir/tcp_probe.txt \
+        -o $dir/cwnd.png
+done
+
+echo "Started at" $start
+echo "Ended at" `date`
+echo "Output saved to $rootdir"
---
+++ topo_assignment/ringsingle.py
@@ -0,0 +1,232 @@
+#!/usr/bin/python
+
+"Assignment 4 - Creates a parking lot topology, \
+    generates flows from senders to the receiver, \
+    measures throughput of each flow"
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.log import lg, output
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import custom, quietRun, dumpNetConnections
+from mininet.cli import CLI
+from mininet.node import OVSSwitch, Controller
+from time import sleep, time
+from multiprocessing import Process
+from subprocess import Popen
+import termcolor as T
+import argparse
+
+import sys
+import os
+from util.monitor import monitor_devs_ng
+
+def cprint(s, color, cr=True):
+    """Print in color
+       s: string to print
+       color: color to use"""
+    if cr:
+        print T.colored(s, color)
+    else:
+        print T.colored(s, color),
+
+parser = argparse.ArgumentParser(description="Parking lot tests")
+parser.add_argument('--bw', '-b',
+                    type=float,
+                    help="Bandwidth of network links",
+                    required=True)
+parser.add_argument('--loss', '-lo',
+                    type=float,
+                    help="loss rate",
+                    required=True)
+parser.add_argument('--delay', '-de',
+
+                    help="Delay",
+                    default="1ms")
+
+parser.add_argument('--dir', '-d',
+                    help="Directory to store outputs",
+                    default="results")
+
+parser.add_argument('-n',
+                    type=int,
+                    help=("Number of senders in the parking lot topo."
+                    "Must be >= 1"),
+                    required=True)
+
+parser.add_argument('--cli', '-c',
+                    action='store_true',
+                    help='Run CLI for topology debugging purposes')
+
+parser.add_argument('--time', '-t',
+                    dest="time",
+                    type=int,
+                    help="Duration of the experiment.",
+                    default=60)
+
+# Expt parameters
+args = parser.parse_args()
+
+if not os.path.exists(args.dir):
+    os.makedirs(args.dir)
+
+lg.setLogLevel('info')
+
+
+class POXBridge(Controller):
+    "Custom Controller class to invoke POX forwarding.l2_learning"
+
+    def start(self):
+        "Start POX learning switch"
+        self.pox = '%s/pox/pox.py' % os.environ['HOME']
+        self.cmd(self.pox, 'forwarding.l2_learning &')
+
+    def stop(self):
+        "Stop POX"
+        self.cmd('kill %' + self.pox)
+
+
+controllers = {'poxbridge': POXBridge}
+# Topology to be instantiated in Mininet
+class RingTopo(Topo):
+    "Parking Lot Topology"
+
+    def __init__(self, n=1, cpu=.1, bw=10, delay=None,
+                 max_queue_size=None,loss =0 , **params):
+        """Parking lot topology with one receiver
+           and n clients.
+           n: number of clients
+           cpu: system fraction for each host
+           bw: link bandwidth in Mb/s
+           delay: link delay (e.g. 10ms)"""
+
+        # Initialize topo
+        Topo.__init__(self, **params)
+
+        # Host and link configuration
+        hconfig = {'cpu': cpu}
+        lconfig = {'bw': bw, 'delay': delay,
+                   'max_queue_size': max_queue_size,
+                   'loss' : loss}
+
+        slist = []
+        for i in range(n):
+            switch = self.addSwitch('s%s' % (i + 1), cls=OVSSwitch)
+            host = self.addHost('h%s' % (i + 1),**hconfig)
+            self.addLink(host, switch)
+            slist.append(switch)
+
+        for i in range(n):
+            if i != n - 1:
+                self.addLink(slist[i], slist[i + 1],**lconfig)
+            else:
+                self.addLink(slist[i], slist[0],**lconfig)
+
+def waitListening(client, server, port):
+    "Wait until server is listening on port"
+    if not 'telnet' in client.cmd('which telnet'):
+        raise Exception('Could not find telnet')
+    cmd = ('sh -c "echo A | telnet -e A %s %s"' %
+           (server.IP(), port))
+    while 'Connected' not in client.cmd(cmd):
+        output('waiting for', server,
+               'to listen on port', port, '\n')
+        sleep(.5)
+
+def progress(t):
+    while t > 0:
+        cprint('  %3d seconds left  \r' % (t), 'cyan', cr=False)
+        t -= 1
+        sys.stdout.flush()
+        sleep(1)
+    print
+
+def start_tcpprobe():
+    os.system("rmmod tcp_probe 1>/dev/null 2>&1; modprobe tcp_probe")
+    Popen("cat /proc/net/tcpprobe > %s/tcp_probe.txt" % args.dir, shell=True)
+
+def stop_tcpprobe():
+    os.system("killall -9 cat; rmmod tcp_probe")
+
+def run_parkinglot_expt(net, n):
+    "Run experiment"
+
+    seconds = args.time
+
+    # Start the bandwidth and cwnd monitors in the background
+    monitor = Process(target=monitor_devs_ng,
+            args=('%s/bwm.txt' % args.dir, 1.0))
+    monitor.start()
+    start_tcpprobe()
+
+    # Get receiver and clients
+    senderlist = []
+
+    recvr = net.getNodeByName('h%s'%(n))
+    port = 5001
+    recvr.cmd('iperf -s -p', port,
+              '> %s/iperf_server.txt' % args.dir, '&')
+    for i in range(n - 1):
+        sender = net.getNodeByName('h%s'%(i + 1))
+        waitListening(sender, recvr, port)
+        sender.sendCmd('iperf -c %s -p %s -t %d -i 1 -yc > %s/iperf_%s.txt' % (recvr.IP(), 5001, seconds, args.dir, sender))
+        senderlist.append(sender)
+        progress(1)
+    for i in range(n - 1):
+        senderlist[i].waitOutput()
+        progress(1)
+    recvr.cmd('kill %iperf')
+    #  Shut down monitors
+    monitor.terminate()
+    stop_tcpprobe()
+
+def check_prereqs():
+    "Check for necessary programs"
+    prereqs = ['telnet', 'bwm-ng', 'iperf', 'ping']
+    for p in prereqs:
+        if not quietRun('which ' + p):
+            raise Exception((
+                'Could not find %s - make sure that it is '
+                'installed and in your $PATH') % p)
+
+def main():
+    "Create and run experiment"
+    start = time()
+    m = args.n
+    topo = RingTopo(n= m)
+    host = custom(CPULimitedHost, cpu=.15)  # 15% of system bandwidth
+    link = custom(TCLink, bw=args.bw, delay=args.delay,loss = args.loss,
+                  max_queue_size=200)
+
+    net = Mininet(topo=topo, host=host, link=TCLink, controller=POXBridge)
+
+    net.start()
+    for i in range(m):
+        net.get('s%s' % (i + 1)).cmd('ovs-vsctl set bridge s%s stp-enable=true' % (i + 1))
+        print "start STP on s%s"%(i + 1)
+    print "sleep 30s for STP"
+    sleep(30)
+    print "wake up"
+    cprint("*** Dumping network connections:", "green")
+    dumpNetConnections(net)
+
+    cprint("*** Testing connectivity", "blue")
+
+    net.pingAll()
+
+    if args.cli:
+        # Run CLI instead of experiment
+        CLI(net)
+    else:
+        cprint("*** Running experiment", "magenta")
+        run_parkinglot_expt(net, n= m)
+
+    net.stop()
+    end = time()
+    os.system("killall -9 bwm-ng")
+    cprint("Experiment took %.3f seconds" % (end - start), "yellow")
+
+if __name__ == '__main__':
+    check_prereqs()
+    main()
