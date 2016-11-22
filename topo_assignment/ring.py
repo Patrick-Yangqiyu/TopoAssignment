10f3c1ff8daa706db30a2c2dee0b115b2e464dfe
yangqiyu <patrick.yangqiyu@gmail.com>
2016-11-22 20:34:15 +0800

    Topo assignment

--- topo_assignment/ring.py
+++ topo_assignment/ring.py
@@ -93,7 +93,7 @@ class RingTopo(Topo):
     "Parking Lot Topology"
 
     def __init__(self, n=1, cpu=.1, bw=10, delay=None,
-                 max_queue_size=None, loss=0, **params):
+                 max_queue_size=None,loss =0 , **params):
         """Parking lot topology with one receiver
            and n clients.
            n: number of clients
@@ -108,7 +108,7 @@ class RingTopo(Topo):
         hconfig = {'cpu': cpu}
         lconfig = {'bw': bw, 'delay': delay,
                    'max_queue_size': max_queue_size,
-                   'loss': loss}
+                   'loss' : loss}
 
         slist = []
         for i in range(n):
@@ -121,7 +121,7 @@ class RingTopo(Topo):
             if i != n - 1:
                 self.addLink(slist[i], slist[i + 1])
             else:
-                self.addLink(slist[i], slist[0],**lconfig)
+                self.addLink(slist[i], slist[0],delay='0ms',loss=1)
 
 def waitListening(client, server, port):
     "Wait until server is listening on port"
@@ -199,7 +199,7 @@ def main():
     link = custom(TCLink, bw=args.bw, delay='0ms',loss = args.loss,
                   max_queue_size=200)
 
-    net = Mininet(topo=topo, controller=POXBridge)
+    net = Mininet(topo=topo, host=host, link=link, controller=POXBridge)
 
     net.start()
     for i in range(m):
