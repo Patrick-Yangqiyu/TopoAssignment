01f8d30c74f8783d21afaadd7cd1546cf2297676
yangqiyu <patrick.yangqiyu@gmail.com>
2016-11-22 21:13:54 +0800

    Topo assignment

--- topo_assignment/ring-single-sweep.sh
+++ topo_assignment/ring-single-sweep.sh
@@ -21,7 +21,7 @@ start=`date`
 exptid=`date +%b%d-%H:%M`
 
 bw=10
-rootdir=Ring-$exptid-Single-Delay$delay-Loss$loss
+rootdir=Ring-$exptid-Single
 # Note: you need to make sure you report the results
 # for the correct port!
 # In this example, we are assuming that each
@@ -30,7 +30,7 @@ for delay in "0ms 1ms 3ms" ;do
     for loss in 0 1 2 ;do
         for n in  4 5 6; do
 
-            dir=$rootdir/n$n
+            dir=$rootdir/Delay$delay-Loss$loss/n$n
             python ringsingle.py --bw $bw \
                 --dir $dir \
                 --delay $delay\
