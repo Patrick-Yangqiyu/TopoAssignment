85e22bdb9807393f9b3c5d4e333ad6ab6c7ba23c
yangqiyu <patrick.yangqiyu@gmail.com>
2016-11-24 19:20:38 +0800

    Topo assignment

--- topo_assignment/ring-sweep.sh
+++ topo_assignment/ring-sweep.sh
@@ -45,7 +45,7 @@ for delay in  0 1 10 ;do
                 -i 's.*-eth1' \
                 -f $dir/bwm.txt \
                 -o $dir/rate.png \
-                --rx 1
+                --rx true
             python util/plot_tcpprobe.py \
                 -f $dir/tcp_probe.txt \
                 -o $dir/cwnd.png
